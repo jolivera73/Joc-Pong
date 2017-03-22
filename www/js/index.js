8f14d41a725c61e48ade5ddc1cc53238258d9e32
jolivera73 <jolivera73@iesnumancia.cat>
2017-03-22 12:33:57 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -92,7 +92,7 @@ function draw(amplada_pantalla_CSS,alcada_pantalla_CSS,posicio_x_bola,posicio_y_
         		html, body {
 					  width:  100%;
 					  height: 100%;
-					  margin: 0px;
+					  margin: 5px;
 				   }
         		
         	</style>	
